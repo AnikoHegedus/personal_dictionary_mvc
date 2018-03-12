f22bec009581d48ead26d58e00aa9ad27a88f4fe
AnikoHegedus <millkamill@hotmail.com>
2018-03-12 10:10:17 +0100

    jquery

--- views/layout.php
+++ views/layout.php
@@ -2,7 +2,11 @@
   <html>
 
   <head>
-  <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js?ver=1.4.2'></script>
+  <script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+ <!--  <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js?ver=1.4.2'></script> -->
     <link rel="stylesheet" href="../style/style.css">
   </head>
 
