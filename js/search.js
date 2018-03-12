abae7ec776be2e9ad789fbaf0846018d851960ff
AnikoHegedus <millkamill@hotmail.com>
2018-03-12 10:06:39 +0100

    test

--- js/search.js
+++ js/search.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
         },
           success: function(data) {
           
-            $("#test2").html(data);
+            $("#test2").html('data');
             
             //console.log(word);
           },
